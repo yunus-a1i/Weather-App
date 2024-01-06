1402df98b8cb875d2f8ec88a133e40d1bf8983c9
Yunus Ali Siddiqui <yunusalisiddiqui90@gmail.com>
2024-01-06 13:43:49 +0530

    Create script.js

---
+++ src/script.js
@@ -0,0 +1,12 @@
+const container = document.querySelector('.container');
+const search = document.querySelector('search-box button');
+const weatherBox = document.querySelector('weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+
+search.addEventListener('click', () => {
+    const APIKey = '00a5ce4e7cc4622fc5996b27490728d4';
+    const city = document.querySelector('.search-box input').value;
+
+
+if(city == '00a5ce4e7cc4622fc5996b27490728d4')
+})
